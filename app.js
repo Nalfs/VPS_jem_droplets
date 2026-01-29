55e1c72003485f052a58c0e8ecedf97d103f06c5
Nadim Al-Sharif <YOUR_GITHUB_EMAIL@example.com>
2026-01-29 19:57:00 +0000

    init

---
+++ app.js
@@ -0,0 +1,10 @@
+const http = require('http');
+
+http.createServer(function (req, res) {
+res.write("on the way to FS!")
+res.end()
+
+}
+).listen(3000);
+
+console.log("Server started on port 3000");
