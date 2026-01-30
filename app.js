4c53c2a913bc8659e3f1e7382125dec1fd4382fa
nadim <nadim.al.sharif@gmail.com>
2026-01-30 10:46:00 +0100

    test

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-const http = require('http');
+const http = require("http");
 
-http.createServer(function (req, res) {
-res.write("on the way to FS!")
-res.end()
-
-}
-).listen(3000);
+http
+  .createServer(function (req, res) {
+    res.write("on the way to FS!");
+    res.end();
+  })
+  .listen(3000);
 
 console.log("Server started on port 3000");
+console.log("test");
